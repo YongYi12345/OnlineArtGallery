0518844411e643cd7a0e423a3e99c69b28b0a2cd
LIM BING HAN DUCIEN 2302493H <2302493H@student.tp.edu.sg>
2025-02-04 11:48:17 +0800

    test

--- OnlineArtGallery/Data/OnlineArtGalleryContext.cs
+++ OnlineArtGallery/Data/OnlineArtGalleryContext.cs
@@ -9,17 +9,27 @@ namespace OnlineArtGallery.Data
     public class OnlineArtGalleryContext(DbContextOptions<OnlineArtGalleryContext> options) : IdentityDbContext<OnlineArtGalleryUser>(options)
     {
 
-        public DbSet<OnlineArtGallery.Domain.User> User { get; set; } = default!;
         public DbSet<OnlineArtGallery.Domain.Artwork> Artwork { get; set; } = default!;
+        public DbSet<OnlineArtGallery.Domain.Wishlist> Wishlist { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.ApplyConfiguration(new ArtworkSeed());
+//<<<<<<< HEAD
+            //builder.ApplyConfiguration(new ArtworkSeed());
             builder.ApplyConfiguration(new AuctionSeed());
+            builder.ApplyConfiguration(new WishlistSeed());
+            builder.ApplyConfiguration(new UserSeed());
         }
         public DbSet<OnlineArtGallery.Domain.Auction> Auction { get; set; } = default!;
-
+        public DbSet<OnlineArtGallery.Domain.Discussion> Discussion { get; set; } = default!;
+        public DbSet<OnlineArtGallery.Domain.Comment> Comment { get; set; } = default!;
+        
+//>>>>>>> 01fc31ebfc7707597681e57ed1dae173ad458af8
+//=======
+           
+        }
+        
+//>>>>>>> 01fc31ebfc7707597681e57ed1dae173ad458af8
     }
-}
